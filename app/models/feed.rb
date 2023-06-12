0de580a023084b8dad3461bb89011e3e7e565077
yuuasanuma <yutoibusousayuhuu@0723@gmail.com>
2023-06-12 10:03:58 +0900

    投稿した際のユーザー表示完了

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,4 +2,5 @@ class Feed < ApplicationRecord
   mount_uploader :image,ImageUploader
 
   validates :image, presence: true
+  belongs_to :user
 end
---
+++ db/migrate/20230612004244_add_user_to_feeds.rb
@@ -0,0 +1,5 @@
+class AddUserToFeeds < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :feeds, :user, null: true, foreign_key: true
+  end
+end
